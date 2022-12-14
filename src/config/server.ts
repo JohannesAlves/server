336e3c54c208adea3a55835554f29b06b5bbe330
JohannesAlves <johannes001002@gmail.com>
2022-12-14 14:49:44 -0300

    chore: config cors & server.

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,6 +1,12 @@
+import cors from "cors";
 import express from "express";
+import routes from "../routes/index";
 
 const app = express();
 const port = 3333;
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 app.listen(port, () => console.log(`Server Running on Port ${port}`));
