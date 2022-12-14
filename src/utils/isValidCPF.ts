817e00ca624bdc863eb5386ed8b6acd11d13a991
JohannesAlves <johannes001002@gmail.com>
2022-12-14 20:00:46 -0300

    feature: check valid CPF.

---
+++ src/utils/isValidCPF.ts
@@ -0,0 +1,39 @@
+export function isValidCPF(cpf: string) {
+    // remove all masks.
+    cpf = cpf.replace(/[^\d]+/g, "");
+
+    // if cpf is empty.
+    if (cpf === "") return false;
+
+    // remove cpfs can't be true.
+    if (
+        cpf.length !== 11 ||
+        cpf === "00000000000" ||
+        cpf === "11111111111" ||
+        cpf === "22222222222" ||
+        cpf === "33333333333" ||
+        cpf === "44444444444" ||
+        cpf === "55555555555" ||
+        cpf === "66666666666" ||
+        cpf === "77777777777" ||
+        cpf === "88888888888" ||
+        cpf === "99999999999"
+    )
+        return false;
+
+    // Validate First Digit.
+    let add = 0;
+    for (let i = 0; i < 9; i++) add += parseInt(cpf.charAt(i)) * (10 - i);
+    let rev = 11 - (add % 11);
+
+    if (rev === 10 || rev === 11) rev = 0;
+    if (rev !== parseInt(cpf.charAt(9))) return false;
+
+    // Validate Second Digit
+    add = 0;
+    for (let i = 0; i < 10; i++) add += parseInt(cpf.charAt(i)) * (11 - i);
+    rev = 11 - (add % 11);
+    if (rev === 10 || rev === 11) rev = 0;
+    if (rev !== parseInt(cpf.charAt(10))) return false;
+    return true;
+}
